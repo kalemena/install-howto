afa7d4ca007587cbd9b40e64a6c2fcfa5c03f0a9
kalemena <kalemena@free.fr>
2019-12-30 20:10:57 +0100

    Switch to adoc

---
+++ video/install.sh
@@ -0,0 +1,12 @@
+
+# Codecs / Transcode
+# https://doc.ubuntu-fr.org/handbrake
+apt-get install handbrake handbrake-cli
+
+# PiTiVi
+apt-get install pitivi
+
+# OpenShot (https://doc.ubuntu-fr.org/openshot)
+apt-get install openshot
+
+
