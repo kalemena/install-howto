ee9769404ed8fc09926adf6b992976735a0f3bf1
kalemena <kalemena@free.fr>
2020-11-20 09:08:09 +0100

    Stacks: tock node example

---
+++ stacks/tock/samples/tock-bot-node/main.js
@@ -0,0 +1,42 @@
+
+const { Bot } = require('tock-node');
+
+const bot = new Bot('e10f4aaf-a7c9-470d-933c-a4b8b08a49c4', 'bot_api', 8080, 'ws');
+
+bot.addStory('allumer la lumière', bot => {
+  console.log("allumer lumière");
+  bot.send("j'allume la lumière");
+//  bot.end('Comment puis-je aider ?');
+});
+
+bot.addStory('allumer', (bot, request) => {
+  console.log("intent allumer");
+  // getting user context
+  console.log(bot.userData);
+  // user message
+  console.log(request.message.text);
+  // entities
+  console.log(request.entities);
+  // answer
+  bot.send("j'allume la lumière");
+//  bot.end('Comment puis-je aider ?');
+});
+
+bot.addStory('greeting',  (bot, request) => {
+  console.log("intent greeting/bonjour");
+  // getting user context
+  console.log(bot.userData);
+  // user message
+  console.log(request.message.text);
+  // entities
+  console.log(request.entities);
+  // answer
+  bot.send("que puis-je pour vous ?");
+//  bot.end('Comment puis-je aider ?');
+});
+
+//     newStory("qui-es-tu") { // Answer for the 'qui-es-tu' story
+//                send("Je suis un assistant conversationnel construit avec Tock")
+//                end("Comment puis-je aider ?")
+//            }
+
